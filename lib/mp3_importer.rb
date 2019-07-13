c30edc890541095efa5a30e7931ff0fb39863063
Thomas Viles <thomas.c.viles.jr@gmail.com>
2019-07-13 20:41:54 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,5 +1,4 @@
 class MP3Importer
-<<<<<<< HEAD
   attr_reader :path
 
   def initialize(path)
@@ -13,6 +12,4 @@ class MP3Importer
   def import
     files.each{|f| Song.new_by_filename(f)}
   end
-=======
->>>>>>> bd3529ab8eaab3334400c75b671c6484a93998ec
 end
